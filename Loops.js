5cf763d4a8058077c5feb852c0ea536a6dafeb7e
Thiago Fontanella <thiago.fontanella@hotmail.com>
2021-03-04 15:21:45 -0300

    finalizando loops e curso

--- Loops.js
+++ Loops.js
@@ -8,7 +8,7 @@ const listaDeDestinos = new Array(
 const idadeComprador = 15; // Declarando variável para trabalhar com ela nos condicionais depois
 const estaAcompanhada = true;
 let passagemComprada = false;
-const destino = "São Paulo";
+const destino = "Salvador";
 const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 let contador = 0;
 let destinoExiste = false;
@@ -27,6 +27,25 @@ while(contador < 3){
 }
 
 console.log("Destino existe? ", destinoExiste)
+
+if(podeComprar && destinoExiste){
+    console.log("Boa viagem");
+} else { 
+    console.log("Desculpe, tivemos erros")
+}
+
+for(let i = 0; i < 3 ; i++){
+    if(listaDeDestinos[contador] == destino){
+        console.log("Destino existe");
+        destinoExiste = true;
+        break;
+    } else {
+        console.log("Destino náo existe")
+        destinoExiste = false;
+    }
+}
+
+
 
     
 
